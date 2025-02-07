262724b35ba3bc37a3a04152d7d18632c00608b2
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-07 23:30:15 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -589,12 +589,12 @@ check_common() {
     local mode="$1"
     case "$mode" in
         full)
+            download_script messages.db
+            download_script country.db
+            download_script openwrt.db
             check_country
             check_openwrt
-            download_script openwrt.db
-            download_script messages.db
             #wget -O /tmp/aios/messages.db https://raw.githubusercontent.com/site-u2023/aios/main/messages.db
-            download_script country.db
             ;;
         light)
             check_country
