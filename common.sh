3e35d96c3bee29f62df654ff8ae17181c453876b
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-07 23:03:31 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -594,12 +594,7 @@ check_common() {
             check_openwrt
             download_script openwrt.db
             #download_script messages.db
-            wget -O https://raw.githubusercontent.com/site-u2023/aios/refs/heads/main/messages.db
-
-            wget -O /tmp/aios/messages.db "https://raw.githubusercontent.com/site-u2023/aios/main/messages.db"
-
-
-
+            wget -O /tmp/aios/messages.db https://raw.githubusercontent.com/site-u2023/aios/main/messages.db
             download_script country.db
             ;;
         light)
