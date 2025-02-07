611f14387227981c4d6403e01735ff920073c22f
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-07 23:33:52 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -57,12 +57,6 @@ packages() {
 delete_aios
 make_directory
 download_common
-messages_db
 check_common full
 packages
 download_script aios
-
-# `messages.db` の存在を確認
-if [ ! -f "${BASE_DIR}/messages.db" ]; then
-    handle_error "messages.db not found after download_script execution."
-fi
