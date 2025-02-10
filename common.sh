806b210558e62fdcb1210567f5acac1b4a18ecec
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-10 16:20:07 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important!　OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.10-0-2"
+COMMON_VERSION="2025.02.10-0-3"
  
 # 基本定数の設定
 # BASE_WGET="wget -O" # テスト用
@@ -180,7 +180,6 @@ select_country() {
         done
     done
 }
-
 
 #########################################################################
 # select_country: アップロードされた common.sh & country.sh OKバージョン
