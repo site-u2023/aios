b9e430ee00e4dfe9668b6335d8e7801d6335dc68
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-11 23:23:05 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.11-6-5"
+COMMON_VERSION="2025.02.11-6-6"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -92,6 +92,9 @@ selection_list() {
 
     echo -n "" > "$list_file"
 
+    # デバッグ用：入力データ確認
+    debug_log "DEBUG: input_data='$input_data'"
+
     echo "[0] Cancel / back to return"
 
     if [ "$mode" = "country" ]; then
@@ -116,7 +119,7 @@ selection_list() {
     fi
 
     # デバッグ用：リスト内容を出力
-    debug_log "Generated selection list:"
+    debug_log "DEBUG: Generated selection list:"
     cat "$list_file"
 
     # 選択入力を受け取る
