cc7d6e1e36afaf65e86ddd77431047241ea75b6b
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-12 21:29:56 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.12-6-12"
+COMMON_VERSION="2025.02.12-6-13"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -161,7 +161,8 @@ selection_list() {
         read yn
         case "$yn" in
             [Yy]*)
-                printf "%s\n" "$selected_value" > "$output_file"
+                # printf "%s\n" "$selected_value" > "$output_file" # #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                echo "$selected_value" > "$output_file"
                 [ -s "$output_file" ] && debug_log "DEBUG: tmp_country written -> $(cat "$output_file")"     #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
                 debug_log "DEBUG: zone_tmp.ch content AFTER extraction -> $(cat "$cache_zone" 2>/dev/null)"
                 return
