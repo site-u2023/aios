a88740ea188c743f5016cb95bec97cd8adc8cb09
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-12 23:19:42 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 # aios.sh (初期エントリースクリプト)
 # License: CC0
-AIOS_VERSION="2025.02.12-3"
+AIOS_VERSION="2025.02.13-0"
 echo -e "\033[7;40maios.sh Updated to version $AIOS_VERSION \033[0m"
 
 # 初期設定
 DEBUG_MODE=false
 RESET_CACHE=false
 SHOW_HELP=false
-INPUT_LANG="${1:-}"  # `$1` をそのままセット（デフォルトなし）
+INPUT_LANG=""
 
-# オプション解析
-while [ $# -gt 0 ]; do
-    case "$1" in
+# 引数解析
+for arg in "$@"; do
+    case "$arg" in
         -d|--debug|-debug) DEBUG_MODE=true ;;
         -reset|--reset|-r) RESET_CACHE=true ;;
         -help|--help|-h) SHOW_HELP=true ;;
+        *)
+            # 言語コードとして処理（最初の未定義引数を言語コードとみなす）
+            if [ -z "$INPUT_LANG" ]; then
+                INPUT_LANG="$arg"
+            fi
+            ;;
     esac
-    shift
 done
 
 export DEBUG_MODE INPUT_LANG  # 環境変数として渡す
@@ -53,30 +58,17 @@ download_common() {
 }
 
 #################################
-# インストール
-#################################
-packages() {
-    install_packages yn ttyd luci-app-ttyd uci
-}
-
-#################################
-# aios の削除
+# メイン処理
 #################################
 delete_aios() {
     rm -rf "${BASE_DIR}" /usr/bin/aios
     echo "Initialized aios"
 }
 
-#################################
-# 必要なディレクトリの作成
-#################################
 mkdir_aios() {
     mkdir -p "$BASE_DIR"
 }
 
-#################################
-# メイン処理
-#################################
 if $SHOW_HELP; then
     print_help
     exit 0
@@ -90,5 +82,3 @@ delete_aios
 mkdir_aios
 download_common
 check_common "full" "$INPUT_LANG"
-packages
-download_script aios
