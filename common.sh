f783feef25f25b7c61a2c68be305d1bdd8e0ea43
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-12 12:12:42 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.12-2-2"
+COMMON_VERSION="2025.02.12-2-3"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -97,7 +97,7 @@ selection_list() {
             local extracted=$(echo "$line" | awk '{print $2, $3, $4, $5}')
             if [ -n "$extracted" ]; then
                 echo "[$i] $extracted"
-                echo "$i $line" >> "$list_file"
+                echo "$i $extracted" >> "$list_file"
                 i=$((i + 1))
             fi
         done
