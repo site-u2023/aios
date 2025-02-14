72d65b36a455f060a75b4de4801a8b005254f53e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-14 17:43:05 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.14-5-14"
+COMMON_VERSION="2025.02.14-5-15"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -352,10 +352,12 @@ selection_list() {
         if [ "$choice" = "0" ]; then
             if [ "$mode" = "country" ]; then
                 printf "%s\n" "$(color yellow "Returning to country selection.")"
-                return 1  # ✅ 言語選択画面に戻る
+                select_country  # ✅ 言語選択に戻る
+                return 0
             elif [ "$mode" = "zone" ]; then
-                printf "%s\n" "$(color yellow "Returning to zone selection.")"
-                return 1  # ✅ ゾーン選択画面に戻る
+                printf "%s\n" "$(color yellow "Returning to language selection.")"
+                select_country "$(cat "$CACHE_DIR/country_tmp.ch" | awk '{print $4}')"  # ✅ 言語選択に戻る
+                return 0
             fi
         fi
 
@@ -385,6 +387,7 @@ selection_list() {
         esac
     done
 }
+
 
 XXX_2014_03_selection_list() {
     local input_data="$1"
