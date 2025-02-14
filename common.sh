7b60910ba9ff2aa654b5431d33f2c120f64e3013
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-14 20:07:08 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.14-10-1"
+COMMON_VERSION="2025.02.14-11-0"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -418,7 +418,7 @@ selection_list() {
             local confirm_info=$(printf "%s\n" "$selected_value" | awk '{print $1, $2}')
         fi
 
-        printf "%s\n" "$(color cyan "Confirm selection: [$choice] $confirm_info (Y/N/R)?")"
+        printf "%s\n" "$(color cyan "Confirm selection: [$choice] $confirm_info")"
         printf "%s" "(Y/N/R)?: "
         read -r yn
 
