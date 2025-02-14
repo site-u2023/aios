7dc07ad5f8100043713aa88f441f5799522ebaac
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-14 21:39:09 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.14-13-2"
+COMMON_VERSION="2025.02.14-13-3"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -330,9 +330,9 @@ selection_list() {
             [Rr]*) check_common
                    return
                    ;;
-            *)     printf "%s\n" $(color red "Invalid input. Please enter 'Y', 'N', or 'R'.")
-                   continue
-                   ;;
+            *) printf "%s\n" $(color red "Invalid input. Please enter 'Y', 'N', or 'R'.")
+               continue
+               ;;
         esac
     done
 }
