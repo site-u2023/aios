432ac90e3a9d7bf928273057c045b2f936452f39
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-15 19:42:58 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-COMMON_VERSION="2025.02.15-6-3"
+COMMON_VERSION="2025.02.15-7-0"
 
 # 基本定数の設定
 BASE_WGET="wget --quiet -O"
@@ -785,7 +785,7 @@ install_package() {
     # `apk` の場合、依存関係のチェック
     if [ "$PACKAGE_MANAGER" = "apk" ]; then
         if ! apk info --depends "$package_name" >/dev/null 2>&1; then
-            echo "Dependency check failed: $package_name has missing dependencies."
+            echo "$(get_message "MSG_PACKAGE_DEPENDENCY_ERROR" | sed "s/{pkg}/$package_name/")"
             return 1
         fi
     fi
