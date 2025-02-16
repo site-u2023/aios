5db1d5b8ffbbc8ee7db55877b97af33d1d3f17d3
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-17 02:40:02 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-SCRIPT_VERSION="2025.02.16-00-08"
+SCRIPT_VERSION="2025.02.16-00-09"
 echo -e "\033[7;40mUpdated to version $SCRIPT_VERSION common.sh \033[0m"
 
 DEV_NULL="${DEV_NULL:-on}"
@@ -1276,7 +1276,8 @@ check_option() {
 check_common() {
     local lang_code="$SELECTED_LANGUAGE"
     
-    return 0 
+    # `MODE` の実際の値を表示
+    echo "🔍 MODE: $MODE"
     
     case "$MODE" in
         reset)
