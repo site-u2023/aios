bdaee3fcbf39113cffb997d995c714ea48d0ce5e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-18 18:09:47 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-SCRIPT_VERSION="2025.02.18-02-01"
+SCRIPT_VERSION="2025.02.18-02-02"
 echo -e "\033[7;40mUpdated to version $SCRIPT_VERSION common.sh \033[0m"
 
 DEV_NULL="${DEV_NULL:-on}"
@@ -571,7 +571,7 @@ select_country() {
             continue
         fi
 
-        selection_list "$search_results" "$tmp_country" "country"
+        select_list "$search_results" "$tmp_country" "country"
 
         # **Y/N 確認時のみ `normalize_input()` を適用**
         local yn
