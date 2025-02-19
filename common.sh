a1d6667de09cbc261725761fcb7857aa7d8443e5
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-19 21:02:14 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-SCRIPT_VERSION="2025.02.19-09-03"
+SCRIPT_VERSION="2025.02.19-09-04"
 echo -e "\033[7;40mUpdated to version $SCRIPT_VERSION common.sh \033[0m"
 
 DEV_NULL="${DEV_NULL:-on}"
@@ -587,7 +587,8 @@ select_country() {
         fi
 
         # **検索結果リストを表示（$2, $3 のみ）**
-        select_list "$search_results" "$tmp_country" "country"
+        #select_list "$search_results" "$tmp_country" "country"
+        select_list "$(echo "$search_results" | awk '{print $2, $3, $4, $5}')" "$tmp_country" "country"
 
         # **選択結果をキャッシュへ書き込み**
         country_write
