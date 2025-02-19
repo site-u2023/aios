75e6d84851d4b3565a5f75d37fe38c0a610f9499
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-19 18:25:35 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-SCRIPT_VERSION="2025.02.19-08-02"
+SCRIPT_VERSION="2025.02.19-08-03"
 echo -e "\033[7;40mUpdated to version $SCRIPT_VERSION common.sh \033[0m"
 
 DEV_NULL="${DEV_NULL:-on}"
@@ -973,9 +973,17 @@ install_package() {
     if [ ! -f "${CACHE_DIR}/update.ch" ]; then
         debug_log "INFO" "Running package manager update..."
         if [ "$PACKAGE_MANAGER" = "opkg" ]; then
-            opkg update > /dev/null 2>&1
+            if opkg update > /dev/null 2>&1; then
+                debug_log "INFO" "opkg update completed successfully."
+            else
+                debug_log "ERROR" "opkg update failed."
+            fi
         elif [ "$PACKAGE_MANAGER" = "apk" ]; then
-            apk update > /dev/null 2>&1
+            if apk update > /dev/null 2>&1; then
+                debug_log "INFO" "apk update completed successfully."
+            else
+                debug_log "ERROR" "apk update failed."
+            fi
         fi
         touch "${CACHE_DIR}/update.ch"
     fi
