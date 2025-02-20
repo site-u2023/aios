0229264bc722d439c99384478637ec959b4cf60c
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-20 17:48:23 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Important! OpenWrt OS only works with Almquist Shell, not Bourne-again shell.
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 
-SCRIPT_VERSION="2025.02.20-11-15"
+SCRIPT_VERSION="2025.02.20-11-16"
 echo -e "\033[7;40mUpdated to version $SCRIPT_VERSION common.sh \033[0m"
 
 DEV_NULL="${DEV_NULL:-on}"
@@ -1169,6 +1169,12 @@ install_package() {
             return 1
         else
             echo "$(get_message "MSG_UPDATE_SUCCESS")"
+            
+            # パッケージが更新された場合のみ表示
+            if [ -n "$file_name" ] && [ -n "$file_version" ]; then
+                echo "$(get_message "MSG_FILE_UPDATED" | sed "s/{file}/$file_name/g" | sed "s/{version}/$file_version/g")"
+            fi
+
             echo "LAST_UPDATE=$(date '+%Y-%m-%d')" > "$update_cache"
         fi
 
