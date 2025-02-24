c0aa4da09d854be3dcc9ddc1af9e00a1a5bf3eea
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-25 02:46:02 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.02.25-00-04"
+SCRIPT_VERSION="2025.02.25-00-05"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
