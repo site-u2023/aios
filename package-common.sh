f1ec7e1fa242c6301debfeef357953625c38346a
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-03 15:22:45 +0900

    Update package-common.sh

--- package-common.sh
+++ package-common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.03.01-01-05"
+SCRIPT_VERSION="2025.03.01-01-06"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
