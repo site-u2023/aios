27879be284f09e0b6b5c07d95b7f1b6d2adfa7bd
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-05 15:32:54 +0900

    Update common-feed-package.sh

--- common-feed-package.sh
+++ common-feed-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.03.05-00-02"
+SCRIPT_VERSION="2025.03.05-00-03"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
