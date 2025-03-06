32d2277129a25251acb86eaa353be039072248e5
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-06 12:49:01 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.03.06-00-06"
+SCRIPT_VERSION="2025.03.06-00-07"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
