dc611f344b6e7ca4488db4ba046aa1e5247c74cc
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-13 16:33:16 +0900

    Update common-country.sh

--- common-country.sh
+++ common-country.sh
@@ -131,9 +131,9 @@ select_country() {
         # 検出された国を表示
         if [ -n "$system_country" ]; then
             # まず検出された国を表示
-            printf "%s %s\n" "$(get_message "MSG_DETECTED_COUNTRY" "$system_country")"
+            printf "%s %s\n" "$(color blue "$(get_message "MSG_DETECTED_COUNTRY" "$system_country")")"
             # 次に確認メッセージを表示
-            printf "%s\n" "$(get_message "MSG_USE_DETECTED_COUNTRY")"
+            printf "%s\n" "$(color blue "$(get_message "MSG_USE_DETECTED_COUNTRY")")"
             # 最後にconfirm関数でYN判定を表示
             if confirm "MSG_CONFIRM_ONLY_YN"; then
                 # country.dbから完全な情報を検索
