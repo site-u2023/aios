098836a2f7d1834d7584d11670b3f76253c99e64
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-21 17:01:28 +0900

    Update common-menu.sh

--- common-menu.sh
+++ common-menu.sh
@@ -211,7 +211,7 @@ display_breadcrumbs() {
     done
     
     # パンくずリストを出力（末尾に空行2つ）
-    printf "%s\n" "$breadcrumb"
+    printf "%s\n\n" "$breadcrumb"
     
     debug_log "DEBUG" "Displayed breadcrumb for submenu with single newline"
 }
