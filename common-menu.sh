d505098d79537e53a8c7031145d7cbd7548266b7
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-21 18:00:42 +0900

    Update common-menu.sh

--- common-menu.sh
+++ common-menu.sh
@@ -681,7 +681,9 @@ selector() {
     local section_name="$1"        # 表示するセクション名
     local parent_display_text="$2" # 未使用（後方互換性のため残す）
     local skip_history="$3"        # 履歴に追加しない場合は1
-  
+
+    printf "\n\n"
+    
     # セクション名が指定されていない場合はメインメニューを使用
     section_name="${section_name:-$MAIN_MENU}"
     
