a132ea59ebb237fe7526e7d333c26b51397b2596
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-25 10:46:35 +0900

    Update package-manual.sh

--- package-manual.sh
+++ package-manual.sh
@@ -51,40 +51,151 @@ LOG_DIR="${LOG_DIR:-$BASE_DIR/logs}"
 FEED_DIR="${FEED_DIR:-$BASE_DIR/feed}"
 DEBUG_MODE="${DEBUG_MODE:-false}"
 
-# パッケージのインストール (オプション)
 packages() {
-    # パッケージのインストール
-    #install_package luci yn hidden
+    install_package luci-i18n-base yn hidden
+    install_package luci-i18n-opkg yn hidden
+    install_package luci-i18n-firewall yn hidden
     install_package ttyd yn hidden
     install_package luci-app-ttyd yn hidden
     install_package luci-i18n-ttyd yn hidden
     install_package openssh-sftp-server yn hidden
     install_package luci-mod-dashboard yn hidden
-    #install_package coreutils yn hidden
+    install_package luci-i18n-dashboard yn hidden
+    install_package coreutils yn hidden
     install_package irqbalance yn hidden
+    install_package luci-app-sqm yn hidden
+    install_package luci-i18n-sqm yn hidden
+    install_package tc-mod-iptables yn hidden
+    install_package luci-app-qos yn hidden
+    install_package luci-i18n-qos yn hidden
+    install_package luci-i18n-statistics yn hidden
+    install_package luci-i18n-nlbwmon yn hidden
+    install_package wifischedule yn hidden
+    install_package luci-app-wifischedule yn hidden
+    install_package luci-i18n-wifischedule yn hidden
+    install_package luci-theme-openwrt yn hidden
+    install_package luci-theme-material yn hidden
+    install_package luci-theme-openwrt-2020 yn hidden
+    install_package attendedsysupgrade-common yn hidden
+    install_package luci-app-attendedsysupgrade yn hidden
+    install_package luci-i18n-attendedsysupgrade yn hidden   
     install_package jq yn hidden
 
-    #feed_package gSpotx2f packages-openwrt current luci-app-cpu-perf yn hidden
-    #feed_package gSpotx2f packages-openwrt current luci-app-cpu-status yn hidden
-    #feed_package gSpotx2f packages-openwrt current luci-app-temp-status yn hidden
-    #feed_package gSpotx2f packages-openwrt current luci-app-log-viewer yn hidden
-    #feed_package gSpotx2f packages-openwrt current luci-app-log yn hidden
-    #feed_package gSpotx2f packages-openwrt current internet-detector yn hidden disabled
+    feed_package gSpotx2f packages-openwrt current luci-app-cpu-perf yn hidden
+    feed_package gSpotx2f packages-openwrt current luci-app-cpu-status yn hidden
+    feed_package gSpotx2f packages-openwrt current luci-app-temp-status yn hidden
+    feed_package gSpotx2f packages-openwrt current luci-app-log-viewer yn hidden
+    feed_package gSpotx2f packages-openwrt current luci-app-log yn hidden
+    feed_package gSpotx2f packages-openwrt current internet-detector yn hidden disabled
 
-    #feed_package_release lisaac luci-app-diskman yn hidden disabled
+    feed_package_release lisaac luci-app-diskman yn hidden disabled
 
-    #feed_package_release jerrykuku luci-theme-argon yn hidden disabled
+    feed_package_release jerrykuku luci-theme-argon yn hidden disabled
+}
+
+packages_19() {
+    install_package luci-i18n-base yn hidden
+    install_package luci-i18n-opkg yn hidden
+    install_package luci-i18n-firewall yn hidden
+    install_package ttyd yn hidden
+    install_package luci-app-ttyd yn hidden
+    install_package luci-i18n-ttyd yn hidden
+    install_package openssh-sftp-server yn hidden
+    install_package luci-mod-dashboard yn hidden
+    install_package luci-i18n-dashboard yn hidden
+    install_package coreutils yn hidden
+    install_package irqbalance yn hidden
+    install_package luci-app-sqm yn hidden
+    install_package luci-i18n-sqm yn hidden
+    install_package tc-mod-iptables yn hidden
+    install_package luci-app-qos yn hidden
+    install_package luci-i18n-qos yn hidden
+    install_package luci-i18n-statistics yn hidden
+    install_package luci-i18n-nlbwmon yn hidden
+    install_package wifischedule yn hidden
+    install_package luci-app-wifischedule yn hidden
+    install_package luci-i18n-wifischedule yn hidden
+    install_package luci-theme-openwrt yn hidden
+    install_package luci-theme-material yn hidden
+    install_package luci-theme-openwrt-2020 yn hidden
+    install_package attendedsysupgrade-common yn hidden
+    install_package luci-app-attendedsysupgrade yn hidden
+    install_package luci-i18n-attendedsysupgrade yn hidden   
+    install_package jq yn hidden
+}
+
+packages_snaphot() {
+    install_package luci yn hidden
+    install_package luci-i18n-base yn hidden
+    install_package luci-i18n-opkg yn hidden
+    install_package luci-i18n-firewall yn hidden
+    install_package ttyd yn hidden
+    install_package luci-app-ttyd yn hidden
+    install_package luci-i18n-ttyd yn hidden
+    install_package openssh-sftp-server yn hidden
+    install_package luci-mod-dashboard yn hidden
+    install_package luci-i18n-dashboard yn hidden
+    install_package coreutils yn hidden
+    install_package irqbalance yn hidden
+    install_package luci-app-sqm yn hidden
+    install_package luci-i18n-sqm yn hidden
+    install_package tc-mod-iptables yn hidden
+    install_package luci-app-qos yn hidden
+    install_package luci-i18n-qos yn hidden
+    install_package luci-i18n-statistics yn hidden
+    install_package luci-i18n-nlbwmon yn hidden
+    install_package wifischedule yn hidden
+    install_package luci-app-wifischedule yn hidden
+    install_package luci-i18n-wifischedule yn hidden
+    install_package luci-theme-openwrt yn hidden
+    install_package luci-theme-material yn hidden
+    install_package luci-theme-openwrt-2020 yn hidden
+    install_package attendedsysupgrade-common yn hidden
+    install_package luci-app-attendedsysupgrade yn hidden
+    install_package luci-i18n-attendedsysupgrade yn hidden   
+    install_package jq yn hidden
+}
+
+install_package_list() {
+    install_package list
+}
+
+packages_usb() {
+    install_package block-mount
+    install_package kmod-usb-storage
+    install_package kmod-usb-storage-uas
+    install_package usbutils
+    install_package gdisk
+    install_package libblkid1
+    install_package kmod-usb-ledtrig-usbport
+    install_package luci-app-ledtrig-usbport
+    install_package dosfstools
+    install_package kmod-fs-vfat
+    install_package e2fsprogs
+    install_package kmod-fs-ext4
+    install_package f2fs-tools
+    install_package kmod-fs-f2fs
+    install_package exfat-fsck
+    install_package kmod-fs-exfat
+    install_package ntfs-3g
+    install_package kmod-fs-ntfs3
+    install_package hfsfsck
+    install_package kmod-fs-hfs
+    install_package kmod-fs-hfsplus
+
+    install_package hdparm
+    install_package hd-idle
+    install_package luci-app-hd-idle
+    install_package luci-i18n-hd-idle
     
-    # install_package list
+    install_package luci-app-samba4
+    install_package luci-i18n-samba4-ja
+    install_package wsdd2
 }
 
 # メイン処理
 main() {
-    #information
-    #set_device_name_password
-    #set_wifi_ssid_password
-    #set_device
-    packages
+
 }
 
 # スクリプトの実行
