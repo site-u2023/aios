bb821312728da5af26c57b413cb5404c01df0358
site_u <140032047+site-u2023@users.noreply.github.com>
2025-03-27 12:08:00 +0900

    Update common-package.sh

--- common-package.sh
+++ common-package.sh
@@ -456,7 +456,6 @@ process_package() {
         debug_log "DEBUG" "Displaying package name: $display_name"
         
         if ! confirm "MSG_CONFIRM_INSTALL" "pkg" "$display_name"; then
-        # if ! confirm "MSG_CONFIRM_INSTALL" "pkg" "$package_name"; then
             debug_log "DEBUG" "User declined installation of $display_name"
             return 0
         fi
