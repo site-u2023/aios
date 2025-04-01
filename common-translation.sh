f80e84973ecbaf54f78596c7bd77207c7f7e7749
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-02 00:56:30 +0900

    Update common-translation.sh

--- common-translation.sh
+++ common-translation.sh
@@ -248,7 +248,7 @@ EOF
     
     # 翻訳処理開始
     printf "\n"
-    printf "Creating translation DB using API: %s\n" "$api_lang"
+    # printf "Creating translation DB using API: %s\n" "$api_lang"
         
     # ネットワーク接続状態を確認
     if [ ! -f "$ip_check_file" ]; then
