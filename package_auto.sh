655778af9f11bdde4311096f4d940abd9f0373c7
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-02 20:34:56 +0900

    Update package_auto.sh

--- package_auto.sh
+++ package_auto.sh
@@ -42,7 +42,7 @@ DEV_NULL="${DEV_NULL:-on}"
 # unset DEV_NULL
 
 # 基本定数の設定 
-BASE_WGET="wget --no-check-certificate -q -O"
+BASE_WGET="wget --no-check-certificate -q"
 # BASE_WGET="wget -O"
 BASE_URL="${BASE_URL:-https://raw.githubusercontent.com/site-u2023/aios/main}"
 BASE_DIR="${BASE_DIR:-/tmp/aios}"
