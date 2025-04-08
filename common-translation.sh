895f3f54d146f72e6ffa13a7a395b4421ba290f4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-08 16:02:23 +0900

    Update common-translation.sh

--- common-translation.sh
+++ common-translation.sh
@@ -61,9 +61,9 @@ GOOGLE_TRANSLATE_URL="${GOOGLE_TRANSLATE_URL:-https://translate.googleapis.com/t
 LIBRETRANSLATE_URL="${LIBRETRANSLATE_URL:-https://translate.argosopentech.com/translate}"
 LINGVA_URL="${LINGVA_URL:-https://lingva.ml/api/v1}"
 # API_LIST="${API_LIST:-google,libretranslate,lingva}"
-# API_LIST="${API_LIST:-lingva}"
+API_LIST="${API_LIST:-lingva}"
 # API_LIST="${API_LIST:-libretranslate}"
-API_LIST="${API_LIST:-google}"
+# API_LIST="${API_LIST:-google}"
 
 # 翻訳キャッシュの初期化
 init_translation_cache() {
