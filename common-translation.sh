fe7adce28e324fbae713c6e6b46aa613e6bde2ed
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-08 10:34:19 +0900

    Update common-translation.sh

--- common-translation.sh
+++ common-translation.sh
@@ -78,25 +78,44 @@ get_api_lang_code() {
     printf "en\n"
 }
 
-# URL安全エンコード関数（効率化版）
+# 最適化版URLエンコード関数
 urlencode() {
     local string="$1"
     local encoded=""
-    local i=0
-    local length=$(printf "%s" "$string" | wc -c)
     
-    while [ $i -lt $length ]; do
-        # より効率的な文字抽出（cut コマンドを使用）
-        local c=$(printf "%s" "$string" | cut -c $(($i+1)))
-        
-        case "$c" in
-            [a-zA-Z0-9.~_-]) encoded="${encoded}$c" ;;
-            " ") encoded="${encoded}%20" ;;
-            *) encoded="${encoded}$(printf "%%%02X" "'$c")" ;;
-        esac
-        
-        i=$(($i + 1))
-    done
+    # 特殊文字の一括変換（高頻度の置換を先に処理）
+    encoded=$(printf "%s" "$string" | sed '
+        s/%/%25/g;
+        s/ /%20/g;
+        s/!/%21/g;
+        s/"/%22/g;
+        s/#/%23/g;
+        s/\$/%24/g;
+        s/&/%26/g;
+        s/'\''/%27/g;
+        s/(/%28/g;
+        s/)/%29/g;
+        s/*/%2A/g;
+        s/+/%2B/g;
+        s/,/%2C/g;
+        s/\//%2F/g;
+        s/:/%3A/g;
+        s/;/%3B/g;
+        s/</%3C/g;
+        s/=/%3D/g;
+        s/>/%3E/g;
+        s/?/%3F/g;
+        s/@/%40/g;
+        s/\[/%5B/g;
+        s/\\/%5C/g;
+        s/\]/%5D/g;
+        s/\^/%5E/g;
+        s/`/%60/g;
+        s/{/%7B/g;
+        s/|/%7C/g;
+        s/}/%7D/g;
+        s/~/%7E/g;
+    ')
     
     printf "%s\n" "$encoded"
 }
