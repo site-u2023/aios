9aa25959f668812efebfa0bb2e5a8f65202e6033
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-09 18:59:07 +0900

    Update common-country.sh

--- common-country.sh
+++ common-country.sh
@@ -293,7 +293,7 @@ select_list() {
         printf "%s\n" "$(color white "$msg_selected")"
         
         # 確認（YNRモードで）
-        confirm "MSG_CONFIRM_SELECT" "type" "ynr"
+        confirm "MSG_CONFIRM_SELECT" "type=ynr"
         local ret=$?
         
         case $ret in
