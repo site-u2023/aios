6fb258fe67583c085438a408d3b18b84c5051ce5
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-11 22:14:27 +0900

    Update common-system.sh

--- common-system.sh
+++ common-system.sh
@@ -610,7 +610,7 @@ detect_wget_capabilities() {
 }
 
 # メイン処理
-common-system_main() {
+common_system_main() {
     detect_cpu_cores
     init_device_cache
     get_usb_devices
@@ -619,4 +619,4 @@ common-system_main() {
 }
 
 # スクリプトの実行
-common-system_main "$@"
+common_system_main "$@"
