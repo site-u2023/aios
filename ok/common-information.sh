6a8e091cc3953bc852f79da7540d9cdc69d2fc7f
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-16 16:22:53 +0900

    Update common-information.sh

--- ok/common-information.sh
+++ ok/common-information.sh
@@ -382,8 +382,8 @@ get_country_code() {
     TIMEZONE_API_SOURCE="" # APIソースは動的に決定
 
     # ユーザーが指定するAPIプロバイダー (デフォルトはcloudflare)
-    API_PROVIDERS="${API_PROVIDERS:-get_country_ipapi get_country_ipinfo}"
-    # API_PROVIDERS="${API_PROVIDERS:-get_country_cloudflare}"
+    #API_PROVIDERS="${API_PROVIDERS:-get_country_ipapi get_country_ipinfo}"
+    API_PROVIDERS="${API_PROVIDERS:-get_country_cloudflare}"
     debug_log "DEBUG" "API_PROVIDERS set to: $API_PROVIDERS"
 
     # キャッシュディレクトリの確認
