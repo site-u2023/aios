dd83c16255005e9fe74572ee252b51a1dd81b639
site_u <140032047+site-u2023@users.noreply.github.com>
2025-04-26 01:00:18 +0900

    Update common-translation.sh

--- common-translation.sh
+++ common-translation.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-SCRIPT_VERSION="2025-04-26-00-03"
+SCRIPT_VERSION="2025-04-26-01-03"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
