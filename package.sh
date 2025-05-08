4ed773e28bcd9da3691fff05094cf158488d3625
site_u <140032047+site-u2023@users.noreply.github.com>
2025-05-08 19:21:59 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.05.08-02-00"
+SCRIPT_VERSION="2025.05.08-02-01"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
