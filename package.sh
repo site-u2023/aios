a33b3d40bebeae7bb0c856f3e6952ea93888c179
site_u <140032047+site-u2023@users.noreply.github.com>
2025-05-10 14:25:20 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.05.10-00-00"
+SCRIPT_VERSION="2025.05.10-00-01"
 
 DEV_NULL="${DEV_NULL:-on}"
 # サイレントモード
@@ -286,8 +286,6 @@ install_packages_version() {
 # メイン処理
 package_main() {
     debug_log "DEBUG" "package_main called. PACKAGE_INSTALL_MODE is currently: '$PACKAGE_INSTALL_MODE'"
-    
-    download "package.db" "hidden"
     
     if [ "$PACKAGE_INSTALL_MODE" = "auto" ]; then
         # common-country.sh の confirm 関数を使用する
