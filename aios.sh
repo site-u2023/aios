2dac9ffbf6526abb5be2a9effef029aabe61ce24
site_u <140032047+site-u2023@users.noreply.github.com>
2025-05-13 03:01:32 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_VERSION="2025.05.10-00-01"
+SCRIPT_VERSION="2025.05.13-00-00"
 
 # =========================================================
 # 📌 OpenWrt / Alpine Linux POSIX-Compliant Shell Script
