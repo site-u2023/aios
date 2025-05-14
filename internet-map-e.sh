54c9b76ae027593a309f628bca9c4f5e5675a438
site_u <140032047+site-u2023@users.noreply.github.com>
2025-05-14 12:33:14 +0900

    Update internet-map-e.sh

--- internet-map-e.sh
+++ internet-map-e.sh
@@ -8,12 +8,6 @@ SCRIPT_VERSION="2025.05.12-05-00"
 . /lib/functions/network.sh
 . /lib/netifd/netifd-proto.sh
 
-# IPv6プレフィックスを取得
-network_flush_cache
-network_find_wan6 NET_IF6
-network_get_ipaddr6 NET_ADDR6 "${NET_IF6}"
-NEW_IP6_PREFIX=${NET_ADDR6}
-
 # プレフィックスに対応するIPv4ベースアドレスを取得（prefix31用）
 get_ruleprefix31_value() {
     local prefix="$1"
