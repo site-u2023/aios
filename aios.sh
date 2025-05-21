25736f76a5eae0fff217282c6d05d07db7eea3b6
site_u <140032047+site-u2023@users.noreply.github.com>
2025-05-21 23:27:06 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -975,7 +975,7 @@ start_spinner() {
             SPINNER_CHARS=". .. ... .... ....."
             ;;
         figure)
-            SPINNER_CHARS="1 2 3 4 5 6 7 8 9 0"
+            SPINNER_CHARS="0 1 2 3 4 5 6 7 8 9"
             ;;
         circle)
             # 環境依存
