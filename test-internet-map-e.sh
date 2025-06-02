d7a003607c77761813061f402cee112a9283a612
site_u <140032047+site-u2023@users.noreply.github.com>
2025-06-03 04:47:18 +0900

    Update test-internet-map-e.sh

--- test-internet-map-e.sh
+++ test-internet-map-e.sh
@@ -731,8 +731,7 @@ get_ruleprefix38_20_value() {
 # ======================================================================
 prompt_for_mape_input() {
     debug_log "DEBUG" "prompt_for_mape_input: Function started."
-
-    printf "Enter the IPv6 prefix or address for MAP-E calculation: "
+    printf "%s\n" "$(color yellow "$(get_message "MENU_INTERNET_MAPE_TEST_MODE")")"
     read -r input_ipv6_prefix
 
     if [ -z "$input_ipv6_prefix" ]; then
