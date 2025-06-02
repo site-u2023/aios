a334678c8a8c8f694b1ee4e6518eeb4c259169b4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-06-03 03:54:28 +0900

    Update test-internet-map-e.sh

--- test-internet-map-e.sh
+++ test-internet-map-e.sh
@@ -2,7 +2,7 @@
 
 # Based on script from https://ipv4.web.fc2.com/map-e.html, with appreciation.
 
-SCRIPT_VERSION="2025.06.03-00-04"
+SCRIPT_VERSION="2025.06.03-00-05"
 
 # OpenWrt関数をロード
 . /lib/functions.sh
