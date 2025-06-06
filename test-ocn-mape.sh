355033486f5e2b1dc778e9f109fbdad4a99e8498
site_u <140032047+site-u2023@users.noreply.github.com>
2025-06-06 20:07:23 +0900

    Update test-ocn-mape.sh

--- test-ocn-mape.sh
+++ test-ocn-mape.sh
@@ -684,7 +684,7 @@ display_mape() {
 
 # メイン処理
 main() {
-    SCRIPT_DEBUG="true"
+    # SCRIPT_DEBUG="true"
     if [ "$SCRIPT_DEBUG" = "true" ]; then
         printf "INFO: Script running in DEBUG mode.\n"
     fi
