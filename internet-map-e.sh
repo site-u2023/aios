070093ede86c0fab995f91c7b632e47b7fe8e950
site_u <140032047+site-u2023@users.noreply.github.com>
2025-06-09 19:48:31 +0900

    Update internet-map-e.sh

--- internet-map-e.sh
+++ internet-map-e.sh
@@ -1381,8 +1381,8 @@ display_mape() {
     printf "export LEGACY=1\n"
     printf "------------------------------------------------------\n"
     printf "\033[34m(config-softwire)#\033[0m \033[1mmap-version draft\033[0m\n"
-    printf "\033[34m(config-softwire)#\033[0m \033[1mrule\033[0m \033[1;34m<0-65535>\033[0m \033[1mipv4-prefix\033[0m \033[1;34m%s/%s\033[0m \033[1mipv6-prefix\033[0m \033[1;34m%s/%s\033[0m [ea-length...]\n" \
-       "$IPV4" "$IP4PREFIXLEN" "$IP6PFX" "$IP6PREFIXLEN"
+    printf "\033[34m(config-softwire)#\033[0m \033[1mrule\033[0m \033[1;34m<0-65535>\033[0m \033[1mipv4-prefix\033[0m \033[1;34m%s/%s\033[0m \033[1mipv6-prefix\033[0m \033[1;34m%s/%s\033[0m [ea-length \033[34m%s\033[0m|psid-length \033[34m%s\033[0m [psid \033[36m%s\033[0m]] [offset \033[34m%s\033[0m] [forwarding]\n" \
+       "$IPV4" "$IP4PREFIXLEN" "$IP6PFX" "$IP6PREFIXLEN" "$EALEN" "$PSIDLEN" "$PSID" "$OFFSET"
     printf "\n"
 
     printf "\n"
