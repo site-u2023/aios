a3b938cb17a3c814d06c5149f5b810bb149d6964
site_u <140032047+site-u2023@users.noreply.github.com>
2025-06-18 13:21:37 +0900

    Update internet-mape.sh

--- internet-mape.sh
+++ internet-mape.sh
@@ -644,6 +644,6 @@ test_internet_map_main() {
 internet_map_ocn_main()       { internet_map_common "ocn" "apply" "$1"; }
 internet_map_main()           { internet_map_common "default" "apply"; }
 
-test_internet_map_main "$@"
+# test_internet_map_main "$@"
 # internet_map_ocn_main "$@"
 # internet_map_main
