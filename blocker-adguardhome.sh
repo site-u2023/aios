48e4e46a41de70d67bac079a3c3f03e48a1fbd7a
site_u <140032047+site-u2023@users.noreply.github.com>
2025-07-17 21:34:41 +0900

    Update blocker-adguardhome.sh

--- blocker-adguardhome.sh
+++ blocker-adguardhome.sh
@@ -186,6 +186,11 @@ common_config_firewall() {
   uci -q delete firewall.adguardhome_dns_53 || true
 
   if command -v nft >/dev/null 2>&1; then
+    nft list table ip nat > /dev/null 2>&1 || nft add table ip nat
+    nft list chain ip nat prerouting > /dev/null 2>&1 || nft add chain ip nat prerouting '{ type nat hook prerouting priority -100; policy accept; }'
+    nft list table ip6 nat > /dev/null 2>&1 || nft add table ip6 nat
+    nft list chain ip6 nat prerouting > /dev/null 2>&1 || nft add chain ip6 nat prerouting '{ type nat hook prerouting priority -100; policy accept; }'
+
     for proto in udp tcp; do
       if ! nft list chain ip nat prerouting 2>/dev/null | grep -qF "iifname \"${LAN}\" ${proto} dport 53 dnat to ${NET_ADDR}:53"; then
         nft add rule ip nat prerouting iifname "${LAN}" ${proto} dport 53 dnat to ${NET_ADDR}:53
@@ -194,13 +199,6 @@ common_config_firewall() {
         nft add rule ip6 nat prerouting iifname "${LAN}" ${proto} dport 53 dnat to ${NET_ADDR6}:53
       fi
     done
-
-    nft list table ip  nat          > /dev/null 2>&1 || nft add table ip  nat
-    nft list chain ip  nat prerouting > /dev/null 2>&1 \
-      || nft add chain ip  nat prerouting '{ type nat hook prerouting priority -100; policy accept; }'
-    nft list table ip6 nat          > /dev/null 2>&1 || nft add table ip6 nat
-    nft list chain ip6 nat prerouting > /dev/null 2>&1 \
-      || nft add chain ip6 nat prerouting '{ type nat hook prerouting priority -100; policy accept; }'
 
     nft list ruleset > /etc/nftables.conf
   else
