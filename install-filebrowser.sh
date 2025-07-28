2e7b03f74a8fb0d4d635e0a9a11692f854147b40
site_u <140032047+site-u2023@users.noreply.github.com>
2025-07-28 12:45:18 +0900

    Update install-filebrowser.sh

--- install-filebrowser.sh
+++ install-filebrowser.sh
@@ -87,7 +87,7 @@ install_filebrowser() {
   trap - EXIT
   
   printf "\033[1;32mFilebrowser installed to %s/filebrowser\033[0m\n" "$INSTALL_DIR"
-  cd "$HOME"
+  cd "$ORIG_PWD"
 }
 
 create_config() {
